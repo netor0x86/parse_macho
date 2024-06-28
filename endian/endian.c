6372e835131de9d7787bf38a5e8be8f36d578fda
Netor0x86 <easyeax@126.com>
2024-06-28 22:20:44 +0800

    增加字节序字节序

---
+++ endian/endian.c
@@ -0,0 +1,66 @@
+//
+// Created by Netor0x86 on 2024/6/28.
+//
+#include "endian.h"
+
+/**
+ * 返回16位小尾数据
+ *
+ * @param value 待处理的数值
+ * @param endian 字节序
+ * @return 如果是小尾，则返回小尾数据，否则返回小尾数据
+ */
+uint16_t big_endian_to_little_endian_uint16(uint16_t value, uint32_t endian)
+{
+    if (endian == LITTLE_ENDIAN)
+    {
+        return value;
+    }
+
+    return (value << 8) | (value >> 8);
+}
+
+/**
+ * 返回32位小尾数据
+ *
+ * @param value 待处理的数值
+ * @param endian 字节序
+ * @return 如果是小尾，则返回小尾数据，否则返回小尾数据
+ */
+uint32_t big_endian_to_little_endian_uint32(uint32_t value, uint32_t endian)
+{
+    if (endian == LITTLE_ENDIAN)
+    {
+        return value;
+    }
+
+    return ((value << 24) & 0xFF000000) |
+           ((value << 8)  & 0x00FF0000) |
+           ((value >> 8)  & 0x0000FF00) |
+           ((value >> 24) & 0x000000FF);
+}
+
+/**
+ * 返回64位小尾数据
+ *
+ * @param value 待处理的数值
+ * @param endian 字节序
+ * @return 如果是小尾，则返回小尾数据，否则返回小尾数据
+ */
+uint64_t big_endian_to_little_endian_uint64(uint64_t value, uint32_t endian)
+{
+    if (endian == LITTLE_ENDIAN)
+    {
+        return value;
+    }
+
+    uint64_t result = 0;
+    uint8_t *pValue = (uint8_t *)&value;
+    uint8_t *pResult = (uint8_t *)&result;
+
+    for (size_t i = 0; i < sizeof(value); ++i) {
+        pResult[i] = pValue[sizeof(value) - 1 - i];
+    }
+
+    return result;
+}
---
+++ endian/endian.h
@@ -0,0 +1,40 @@
+//
+// Created by Netor0x86 on 2024/6/28.
+//
+
+#ifndef CSTUDY_ENDIAN_H
+#define CSTUDY_ENDIAN_H
+
+#include "../macho/macho64.h"
+
+#define LITTLE_ENDIAN 0x01
+#define BIG_ENDIAN    0x02
+
+/**
+ * 返回16位小尾数据
+ *
+ * @param value 待处理的数值
+ * @param endian 字节序
+ * @return 如果是小尾，则返回小尾数据，否则返回小尾数据
+ */
+uint16_t big_endian_to_little_endian_uint16(uint16_t value, uint32_t endian);
+
+/**
+ * 返回32位小尾数据
+ *
+ * @param value 待处理的数值
+ * @param endian 字节序
+ * @return 如果是小尾，则返回小尾数据，否则返回小尾数据
+ */
+uint32_t big_endian_to_little_endian_uint32(uint32_t value, uint32_t endian);
+
+/**
+ * 返回64位小尾数据
+ *
+ * @param value 待处理的数值
+ * @param endian 字节序
+ * @return 如果是小尾，则返回小尾数据，否则返回小尾数据
+ */
+uint64_t big_endian_to_little_endian_uint64(uint64_t value, uint32_t endian);
+
+#endif //CSTUDY_ENDIAN_H
